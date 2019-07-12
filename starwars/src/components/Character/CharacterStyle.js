aaba83f083d394026b8b8da3b3cf78aff9061e23
Sunil Karki <karkisunil1200@gmail.com>
2019-07-12 11:25:16 -0500

    Added styled Component

---
+++ starwars/src/components/Character/CharacterStyle.js
@@ -0,0 +1,10 @@
+import styled, {css} from 'styled-components';
+
+export const Characters = styled.div`
+  box-shadow: 0 5px 10px orangered;
+  height: 300px;
+  width: 300px;
+  background: rgb(241, 240, 240);
+  margin: 10px;
+  padding-top: 5px;
+`;
